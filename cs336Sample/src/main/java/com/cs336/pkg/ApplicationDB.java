4a838ba3c4cd109f32b9c6903e925b8621b54814
jasonz-li <jasonli.0820@yahoo.com>
2021-04-13 16:52:57 -0400

    fdsgsdfg

--- cs336Sample/src/main/java/com/cs336/pkg/ApplicationDB.java
+++ cs336Sample/src/main/java/com/cs336/pkg/ApplicationDB.java
@@ -31,7 +31,7 @@ public class ApplicationDB {
 		}
 		try {
 			//Create a connection to your DB
-			connection = DriverManager.getConnection(connectionUrl,"root", "p][Fu{Hk68B(s{cR");
+			connection = DriverManager.getConnection(connectionUrl,"root", "adsfbsdah");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
